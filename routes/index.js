e2288378c10c0692ae8c47e83228d8e6f7bd06ad
mrjackmaster <jacektrg+github@gmail.com>
2021-12-09 16:44:23 +0100

    Sorting messages by date.

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 const messages = [
 	{
 		user: 'Amando',
-		added: new Date(),
+		date: new Date(),
 		text: 'Hi there!',
 	},
 	{
 		user: 'Charles',
-		added: new Date(),
+		date: new Date(),
 		text: 'Hello World!',
 	},
 ];
@@ -22,13 +22,16 @@ router.post('/new', (req, res, next) => {
 	messages.push({
 		text: req.body[`user-text`],
 		user: req.body[`user-name`],
-		added: new Date(),
+		date: new Date(),
 	});
 	res.redirect('/');
 });
 
 router.get('/', (req, res, next) => {
-	res.render('index', { title: 'Mini Message Board', messages: messages });
+	const sorted = messages.sort((a, b) => {
+		return new Date(b.date) - new Date(a.date);
+	});
+	res.render('index', { title: 'Mini Message Board', messages: sorted });
 });
 
 module.exports = router;
